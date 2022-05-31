b27dd69830dde72f35226eed68fc532d270d2530
Johannes Kopton <johannes@kopton.org>
2022-05-31 16:22:33 +0200

    Initial commit. Baklava.js works perfectly.

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  env: {
+    browser: true
+  },
+  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+  extends: ["plugin:vue/strongly-recommended"],
+  // required to lint *.vue files
+  plugins: ["vue"],
+  settings: {
+    // check if imports actually resolve
+    "import/resolver": {
+      webpack: {
+        config: "build/webpack.base.conf.js"
+      }
+    }
+  },
+  rules: {
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "always",
+          normal: "always",
+          component: "always"
+        },
+        svg: "always",
+        math: "always"
+      }
+    ]
+  }
+};
