d23bdd1fef0a4bb9e6c41f7f847041c22381d59b
Johannes Kopton <johannes@kopton.org>
2022-06-01 18:51:02 +0200

    Remove unnecessary text input from display node.

--- src/DisplayNode.js
+++ src/DisplayNode.js
@@ -4,7 +4,6 @@ export const DisplayNode = new NodeBuilder("DisplayNode")
   .setName("Display")
   .addInputInterface("Value")
   .addOption("ValueText", "TextOption")
-  .addOption("Test", "InputOption")
   .onCalculate(n => {
     let value = n.getInterface("Value").value;
     if (typeof value === "number") {
