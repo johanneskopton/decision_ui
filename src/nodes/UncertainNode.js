d6b79db82b65f120b732af7c98dc183fee91db63
Johannes Kopton <johannes@kopton.org>
2022-06-10 17:01:05 +0200

    Let the uncertain node base class do all the monte carlo stuff so that the individual node classes can be implemented simply as if it was deterministic.

--- src/nodes/UncertainNode.js
+++ src/nodes/UncertainNode.js
@@ -3,12 +3,26 @@ import { NumericNode } from "./NumericNode";
 export class UncertainNode extends NumericNode {
   constructor() {
     super();
+    this.getInterface("Result").type = "probabilistic";
   }
 
   calculate() {
     var result = [];
+    var input_interfaces = this.inputInterfaces;
+    var input_values = new Object();
+    Object.keys(input_interfaces).forEach(e => {
+      input_values[e] = input_interfaces[e].value;
+    });
     for (var i = 0; i < 10; i++) {
-      result.push(this.calculate_single());
+      var input_values_sample = new Object();
+      Object.keys(input_values).forEach(e => {
+        if (input_values[e].length) {
+          input_values_sample[e] = input_values[e][i];
+        } else {
+          input_values_sample[e] = input_values[e];
+        }
+      });
+      result.push(this.calculate_single(input_values_sample));
     }
 
     this.getInterface("Result").value = result;
