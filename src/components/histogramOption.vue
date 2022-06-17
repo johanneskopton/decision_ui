f45a5fbe6ef0e23f3930ba02aeb7dac26b90f55a
Johannes Kopton <johannes@kopton.org>
2022-06-17 14:46:17 +0200

    Make histogram using the chartjs barplot function and a histogram creation function from github (apache licensed).

--- src/components/histogramOption.vue
+++ src/components/histogramOption.vue
@@ -1,52 +1,131 @@
 <template>
-  <canvas id="hist" ref="hist" width="400" height="400" />
+  <canvas id="hist" ref="hist" width="181" max-height="140" />
 </template>
 
 <script>
   import Chart from "chart.js";
+  import histogram from "../histogram-pretty";
+
   export default {
     data() {
       return {};
     },
     mounted() {
       var ctx = this.$refs.hist.getContext("2d");
-      var myChart = new Chart(ctx, {
-        type: "bar",
+      var data = [
+        2,
+        3,
+        6,
+        7,
+        9,
+        5,
+        3,
+        4,
+        6,
+        8,
+        6,
+        5,
+        3,
+        3,
+        2,
+        2,
+        2,
+        11,
+        4,
+        6,
+        8,
+        0,
+        8,
+        6,
+        3,
+        5,
+        7,
+        5,
+        4,
+        2,
+        2,
+        6,
+        8,
+        9,
+        6,
+        4,
+        2,
+        2,
+        2,
+        22,
+        2,
+        5,
+        9,
+        0
+      ];
+
+      function range(start, end, step = 1) {
+        const len = Math.floor((end - start) / step) + 1;
+        return Array(len)
+          .fill()
+          .map((_, idx) => start + idx * step);
+      }
+
+      var hist = histogram(data);
+      var binRange = hist.tickRange();
+      var labels = range(binRange[0], Math.round(binRange[1]), binRange[2]);
+      var bins = Array(labels.length).fill(0);
+
+      for (var i = 0; i < data.length; i++) {
+        var value = data[i];
+        var index = labels.indexOf(hist.fun(value));
+        bins[index] = bins[index] + 1;
+      }
+
+      var baseColor = "rgba(255, 255, 255, 1)";
+      var baseColor2 = "rgba(255, 255, 255, 0.2)";
+
+      var myBarChart = Chart.Bar(ctx, {
         data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          labels: labels,
           datasets: [
             {
-              label: "# of Votes",
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)"
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)"
-              ],
-              borderWidth: 1
+              data: bins,
+              categoryPercentage: 1.0,
+              barPercentage: 1.0,
+              backgroundColor: "#CE93D8"
             }
           ]
         },
         options: {
           scales: {
+            xAxes: [
+              {
+                ticks: {
+                  maxRotation: 0,
+                  minRotation: 0,
+                  autoSkip: true,
+                  maxTicksLimit: 7,
+                  fontColor: baseColor
+                },
+                gridLines: {
+                  color: baseColor2,
+                  zeroLineColor: baseColor
+                }
+              }
+            ],
             yAxes: [
               {
                 ticks: {
-                  beginAtZero: true
+                  fontColor: baseColor
+                },
+                gridLines: {
+                  color: baseColor2,
+                  zeroLineColor: baseColor
                 }
               }
             ]
+          },
+          legend: {
+            display: false
+          },
+          tooltips: {
+            enabled: false
           }
         }
       });
