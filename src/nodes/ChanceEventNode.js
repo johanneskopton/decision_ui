d6f0a045cbeaf217ffe53f961045525f30e99523
Johannes Kopton <johannes@kopton.org>
2022-06-20 14:42:18 +0200

    Update chanceEvent node to inherit from uncertainNode.

--- src/nodes/ChanceEventNode.js
+++ src/nodes/ChanceEventNode.js
@@ -1,24 +1,29 @@
-import { Node } from "@baklavajs/core";
+import { UncertainNode } from "./UncertainNode";
 
-export class ChanceEventNode extends Node {
+export class ChanceEventNode extends UncertainNode {
   constructor() {
     super();
     this.type = "ChanceEventNode";
     this.name = "ChanceEvent";
-    this.addInputInterface("chance", "NumberOption", 0, { min: 0, max: 1 });
-    this.addInputInterface("value_if", "NumberOption", 1);
-    this.addInputInterface("value_if_not", "NumberOption", 0);
-    this.addOutputInterface("Result");
+    this.addInputInterface("chance", "NumberOption", 0, {
+      min: 0,
+      max: 1,
+      type: "deterministic"
+    });
+    this.addInputInterface("value_if", "NumberOption", 1, {
+      type: "probabilistic"
+    });
+    this.addInputInterface("value_if_not", "NumberOption", 0, {
+      type: "probabilistic"
+    });
   }
 
-  calculate() {
-    const chance = this.getInterface("chance").value;
-    const value_if = this.getInterface("value_if").value;
-    const value_if_not = this.getInterface("value_if_not").value;
+  calculate_single(input) {
     var result;
     // short for if ...
     result =
-      (value_if - value_if_not) * (Math.random() < chance) + value_if_not;
-    this.getInterface("Result").value = result;
+      (input.value_if - input.value_if_not) * (Math.random() < input.chance) +
+      input.value_if_not;
+    return result;
   }
 }
