16a8a167f9f9e057eb9e55b8ce784e7fbf571054
Johannes Kopton <johannes@kopton.org>
2022-06-29 15:27:03 +0200

    Further elaborate raw model specification.

--- backend/decision_backend/model.py
+++ backend/decision_backend/model.py
@@ -2,8 +2,24 @@ from pydantic import BaseModel
 from typing import List, TypedDict
 
 
+Connection = TypedDict("connection", {"id": str, "from": str, "to": str})
+
+
+class RawNode(BaseModel):
+    type: str
+    id: str
+    name: str
+    options: List
+    state: TypedDict("state")
+    interfaces: List
+    position: TypedDict("position", {"x": int, "y": int})
+    width: int
+    twoColumn: bool
+    customClasses: str
+
+
 class RawModel(BaseModel):
-    nodes: List
-    connections: List
+    nodes: List[RawNode]
+    connections: List[Connection]
     panning: TypedDict("panning", {"x": int, "y": int})
     scaling: float
