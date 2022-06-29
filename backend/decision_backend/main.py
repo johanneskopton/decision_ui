b93c9d918033c7070e87f4f7ac0c84450b38ccc3
Johannes Kopton <johannes@kopton.org>
2022-06-29 19:55:06 +0200

    Allow cross-origin-resource-sharing (CORS) from frontend.

--- backend/decision_backend/main.py
+++ backend/decision_backend/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from decision_backend.model import RawModel
 from decision_backend.decision_support_wrapper import DecisionSupportWrapper
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/api/v1/decision_support")
