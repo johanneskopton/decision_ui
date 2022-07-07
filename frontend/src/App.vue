0b1ca8c480d6d2b23f6925d646899bf796faa339
Johannes Kopton <johannes@kopton.org>
2022-07-07 16:23:39 +0200

    Add navigation drawer.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,45 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      class="pt-4"
+      color="grey lighten-3"
+      expand-on-hover
+      permanent
+    >
+      <v-list dense nav>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon :rotate="90">
+              mdi-graph mdi-rotate-90
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>
+            Model editor
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>
+              mdi-chart-histogram
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>
+            Results Dashboard
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+      <!--
+      <v-avatar
+        v-for="n in 6"
+        :key="n"
+        :color="`grey ${n === 1 ? 'darken' : 'lighten'}-1`"
+        :size="n === 1 ? 36 : 20"
+        class="d-block text-center mx-auto mb-9"
+      />
+      -->
+    </v-navigation-drawer>
     <div class="main-window">
       <router-view />
     </div>
@@ -8,6 +48,19 @@
 
 <script>
   export default {
-    components: {}
+    components: {},
+    data() {
+      return {
+        drawer: false
+      };
+    }
   };
 </script>
+
+<style>
+  div.main-window {
+    position: absolute;
+    left: 56px;
+    right: 0px;
+  }
+</style>
