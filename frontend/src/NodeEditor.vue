5039d78dc9770b868b7537a345bde4e2ea1c6ef4
Johannes Kopton <johannes@kopton.org>
2022-07-12 17:24:02 +0200

    Initialize model in nodeEditor and not in modelStore. This uses reactivity.

--- frontend/src/NodeEditor.vue
+++ frontend/src/NodeEditor.vue
@@ -84,6 +84,16 @@
   import { saveAs } from "file-saver";
   import axios from "axios";
 
+  import { InterfaceTypePlugin } from "@baklavajs/plugin-interface-types";
+  import { OptionPlugin } from "@baklavajs/plugin-options-vue";
+  import HistogramOption from "./components/histogramOption.vue";
+  import colors from "vuetify/lib/util/colors";
+  import { MathNode } from "./nodes/MathNode";
+  import { SumNode } from "./nodes/SumNode";
+  import { DisplayNode } from "./nodes/DisplayNode";
+  import { UVNode } from "./nodes/UVNode";
+  import { ChanceEventNode } from "./nodes/ChanceEventNode";
+
   export default {
     //components: { HintOverlay },
     data() {
@@ -91,14 +101,61 @@
     },
     created() {
       if (!this.$store.state.model.isInitialized) {
-        this.$store.dispatch("initModel");
+        // Register the plugins
+        // The view plugin is used for rendering the nodes
+        this.$store.state.model.editor.use(this.$store.state.model.viewPlugin);
+        // The option plugin provides some default option UI elements
+        this.$store.state.model.editor.use(new OptionPlugin());
+        // The engine plugin calculates the nodes in the graph in the
+        // correct order using the "calculate" methods of the nodes
+        this.$store.state.model.editor.use(this.$store.state.model.engine);
+        // The interface type plugin allows for custom interface types
+        const intfTypePlugin = new InterfaceTypePlugin();
+        this.$store.state.model.editor.use(intfTypePlugin);
+        // Define interface types
+        intfTypePlugin.addType("probabilistic", colors.purple.accent1);
+        intfTypePlugin.addType("deterministic", colors.shades.white);
+
+        // Show a minimap in the top right corner
+        this.$store.state.model.viewPlugin.enableMinimap = false;
+
+        // register the nodes we have defined, so they can be
+        // added by the user as well as saved & loaded.
+        this.$store.state.model.editor.registerNodeType("Math", MathNode);
+        this.$store.state.model.editor.registerNodeType("Display", DisplayNode);
+        this.$store.state.model.editor.registerNodeType(
+          "UncertainInput",
+          UVNode
+        );
+        this.$store.state.model.editor.registerNodeType("Sum", SumNode);
+        this.$store.state.model.editor.registerNodeType(
+          "ChanceEvent",
+          ChanceEventNode
+        );
+
+        // register custom options
+        this.$store.state.model.viewPlugin.registerOption(
+          "HistogramOption",
+          HistogramOption
+        );
+
+        // add some nodes so the screen is not empty on startup
+
+        const node1 = this.addNodeWithCoordinates(MathNode, 100, 140);
+        const node2 = this.addNodeWithCoordinates(DisplayNode, 400, 140);
+        this.$store.state.model.editor.addConnection(
+          node1.getInterface("Result"),
+          node2.getInterface("Value")
+        );
+
+        this.$store.commit("setInitialized");
       }
       this.$store.state.model.engine.calculate();
     },
     methods: {
       addNodeWithCoordinates(nodeType, x, y) {
         const n = new nodeType();
-        this.editor.addNode(n);
+        this.$store.state.model.editor.addNode(n);
         n.position.x = x;
         n.position.y = y;
         return n;
