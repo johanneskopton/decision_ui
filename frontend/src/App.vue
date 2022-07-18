9d809ff24ce7c178ad623da0828c8aa5dd54f880
Johannes Kopton <johannes@kopton.org>
2022-07-18 13:05:39 +0200

    Move font import and basic css from index.html header to app.vue style.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -61,6 +61,15 @@
 </script>
 
 <style>
+  @import url("https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900");
+  @import url("https://fonts.googleapis.com/css?family=Roboto+Mono:400");
+  @import url("https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css");
+
+  body {
+    margin: 0;
+    font-family: Roboto !important;
+  }
+
   div.main-window {
     position: absolute;
     left: 56px;
