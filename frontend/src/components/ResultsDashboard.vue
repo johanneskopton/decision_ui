7ff8f70186e9b709bdcfb3248c5f1a8c3f7822d2
Johannes Kopton <johannes@kopton.org>
2022-07-18 13:10:04 +0200

    Remove obsolete (and commented out) css code.

--- frontend/src/components/ResultsDashboard.vue
+++ frontend/src/components/ResultsDashboard.vue
@@ -15,11 +15,6 @@
   @import "vuetify/src/styles/main.sass";
   div.results_dashboard {
     background-color: map-get($grey, "lighten-4");
-    /*position: absolute;
-    left: 0px;
-    top: 0px;
-    width: 100%;
-    height: 100%;*/
     width: 100%;
     height: 100vh;
   }
