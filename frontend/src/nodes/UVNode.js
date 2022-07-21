932c1033e849de8ff85623b1747a3162e4afc7e8
Johannes Kopton <johannes@kopton.org>
2022-07-21 13:57:36 +0200

    Initialize the "upper" value of input nodes with 1 instead of 0. (So that they are not initialized with an error.)

--- frontend/src/nodes/UVNode.js
+++ frontend/src/nodes/UVNode.js
@@ -27,9 +27,14 @@ export class UVNode extends UncertainNode {
       this.uv_type = UVType[this.getOptionValue("Probability distribution")];
       this.uv_type.params.forEach(element => {
         if (!old_uv_type || !old_uv_type.params.includes(element)) {
-          this.addInputInterface(element, "NumberOption", 0, {
-            type: "deterministic"
-          });
+          this.addInputInterface(
+            element,
+            "NumberOption",
+            (element == "upper") * 1,
+            {
+              type: "deterministic"
+            }
+          );
         }
       });
       if (old_uv_type) {
