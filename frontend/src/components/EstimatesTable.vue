c1f9a24bfc33b9549f1bf7ca5ccb967b1d6d2ba2
Johannes Kopton <johannes@kopton.org>
2022-07-27 19:13:50 +0200

    Complete #5: Make estimate-nodes/table reactive in both directions.

--- frontend/src/components/EstimatesTable.vue
+++ frontend/src/components/EstimatesTable.vue
@@ -3,15 +3,14 @@
     <vue-excel-editor
       v-if="estimatesData.length > 0"
       v-model="estimatesData"
-      readonly
       width="100%"
     >
-      <vue-excel-column field="label" label="label" />
-      <vue-excel-column field="variable" label="variable" />
-      <vue-excel-column field="distribution" label="distribution" />
-      <vue-excel-column field="lower" label="lower" />
-      <vue-excel-column field="median" label="median" />
-      <vue-excel-column field="upper" label="upper" />
+      <vue-excel-column readonly field="label" label="label" />
+      <vue-excel-column readonly field="variable" label="variable" />
+      <vue-excel-column readonly field="distribution" label="distribution" />
+      <vue-excel-column :change="onLowerChange" field="lower" label="lower" />
+      <vue-excel-column readonly field="median" label="median" />
+      <vue-excel-column :change="onUpperChange" field="upper" label="upper" />
     </vue-excel-editor>
     <v-alert v-else type="info" elevation="2">
       No estimates yet..
@@ -23,6 +22,24 @@
     computed: {
       estimatesData: function() {
         return this.$store.state.model.estimates;
+      }
+    },
+    methods: {
+      onLowerChange: function(newVal, oldVal, row) {
+        this.$store.state.model.editor.nodes.forEach(node => {
+          if (node.type == "Estimate" && node.name == row.variable) {
+            node.options.get("lower").value = Number(newVal);
+            node.calculate_single();
+          }
+        });
+      },
+      onUpperChange: function(newVal, oldVal, row) {
+        this.$store.state.model.editor.nodes.forEach(node => {
+          if (node.type == "Estimate" && node.name == row.variable) {
+            node.options.get("upper").value = Number(newVal);
+            node.calculate_single();
+          }
+        });
       }
     }
   };
