fbd51cde3da9eb194288318fbf05bf87190d3b05
Johannes Kopton <johannes@kopton.org>
2022-08-03 17:12:40 +0200

    Remove "timestep" option, if toSeries node is set to "every".

--- frontend/src/nodes/ToSeriesNode.js
+++ frontend/src/nodes/ToSeriesNode.js
@@ -12,12 +12,31 @@ export class ToSeriesNode extends UncertainSeriesNode {
     this.addInputInterface("n", "IntegerOption", 10, {
       type: "deterministic_int"
     });
-    this.addInputInterface("timestep", "IntegerOption", 0, {
-      type: "deterministic_int"
-    });
+    this.timestep_option = false;
     this.addOption("TimestepMethod", "SelectOption", "every", undefined, {
       items: ["every", "as defined"]
     });
+    this.check_timestep_method = this.check_timestep_method.bind(this);
+    this.check_timestep_method();
+    this.events.update.addListener(undefined, this.check_timestep_method);
+  }
+
+  check_timestep_method() {
+    if (
+      this.getOptionValue("TimestepMethod") == "as defined" &&
+      !this.timestep_option
+    ) {
+      this.timestep_option = true;
+      this.addInputInterface("timestep", "IntegerOption", 0, {
+        type: "deterministic_int"
+      });
+    } else if (
+      this.getOptionValue("TimestepMethod") == "every" &&
+      this.timestep_option
+    ) {
+      this.removeInterface("timestep");
+      this.timestep_option = false;
+    }
   }
 
   calculate_single(input) {
