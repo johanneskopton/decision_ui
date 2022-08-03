0b4555aa999a4f0ddf821d367954672f55afbd59
Johannes Kopton <johannes@kopton.org>
2022-08-03 17:14:03 +0200

    Reorganise mc run number propagation in frontend.

--- frontend/src/nodes/UncertainNode.js
+++ frontend/src/nodes/UncertainNode.js
@@ -1,5 +1,7 @@
 import { NumericNode } from "./NumericNode";
 
+const MC_RUNS = 1000;
+
 export class UncertainNode extends NumericNode {
   constructor() {
     super();
@@ -9,21 +11,22 @@ export class UncertainNode extends NumericNode {
   calculate() {
     if (typeof this._calculate === "function") {
       // additional operations, that only need to be executed once on every change
+      // not every mc run
       this._calculate();
     }
-    var result = this.calculate_scalar();
+    var result = this.calculate_scalar(MC_RUNS);
     this.getInterface("Result").value = result;
     this.check_valid();
   }
 
-  calculate_scalar() {
+  calculate_scalar(mc_runs) {
     var result = [];
     var input_interfaces = this.inputInterfaces;
     var input_values = new Object();
     Object.keys(input_interfaces).forEach(e => {
       input_values[e] = input_interfaces[e].value;
     });
-    for (var i = 0; i < 1000; i++) {
+    for (var i = 0; i < mc_runs; i++) {
       var input_values_sample = new Object();
       Object.keys(input_values).forEach(e => {
         if (input_values[e] === null) {
