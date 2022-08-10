d413d54a097b97b0552827fb0ed06fe4b164dc05
Johannes Kopton <johannes@kopton.org>
2022-08-10 17:16:43 +0200

    Refactor app structure to allow for views unrelated to all the modeling stuff, which is now called "workspace".

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,86 +1,6 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      class="pt-4"
-      color="grey lighten-3"
-      mini-variant
-      permanent
-    >
-      <v-list flat dense nav>
-        <v-list-item-group v-model="selectedItem" color="primary accent-1">
-          <v-list-item to="/modeling" link>
-            <v-list-item-icon>
-              <v-icon :rotate="90">
-                mdi-graph mdi-rotate-90
-              </v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              Model editor
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item to="/estimates" link>
-            <v-list-item-icon>
-              <v-icon>
-                mdi-table
-              </v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              Estimates
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item to="/results" link>
-            <v-list-item-icon>
-              <v-icon>
-                mdi-chart-histogram
-              </v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              Results dashboard
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item to="/code" link>
-            <v-list-item-icon>
-              <v-icon>
-                mdi-language-r
-              </v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              R-Code
-            </v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-      <!--
-      <v-avatar
-        v-for="n in 6"
-        :key="n"
-        :color="`grey ${n === 1 ? 'darken' : 'lighten'}-1`"
-        :size="n === 1 ? 36 : 20"
-        class="d-block text-center mx-auto mb-9"
-      />
-      -->
-    </v-navigation-drawer>
-    <div class="main-window">
-      <router-view />
-      <RunButton />
-    </div>
-  </v-app>
+  <v-app><router-view /></v-app>
 </template>
-
-<script>
-  import RunButton from "./components/RunButton.vue";
-  export default {
-    components: { RunButton },
-    data() {
-      return {
-        drawer: false,
-        selectedItem: false
-      };
-    }
-  };
-</script>
 
 <style>
   @import url("https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900");
@@ -90,25 +10,5 @@
   body {
     margin: 0;
     font-family: Roboto !important;
-  }
-
-  div.main-window {
-    position: absolute;
-    left: 56px;
-    right: 0px;
-    display: flex;
-  }
-  .theme--light.v-icon {
-    color: #9e9e9e;
-  }
-
-  .floating_btn_group {
-    position: absolute;
-    bottom: 16px;
-    z-index: 5;
-  }
-
-  .floating_btn_group.left {
-    left: 16px;
   }
 </style>
