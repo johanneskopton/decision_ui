4211865ff2b7febcaa6abcc33d932dbb9912db30
Johannes Kopton <johannes@kopton.org>
2022-08-17 15:23:53 +0200

    Adllow access from no localhost.

--- backend/decision_backend/main.py
+++ backend/decision_backend/main.py
@@ -26,13 +26,9 @@ async def get_db():
         await db.close()
 
 
-origins = [
-    "http://localhost:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
