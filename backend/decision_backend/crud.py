13391d666ce0f255861fc7222da669fe3aac21a3
Johannes Kopton <johannes@kopton.org>
2022-08-17 12:17:12 +0200

    Solve #46: Update saved model if already exists.

--- backend/decision_backend/crud.py
+++ backend/decision_backend/crud.py
@@ -20,9 +20,20 @@ async def create_user_decision_model(
         db: Session,
         decision_model: schemas.DecisionModelCreate,
         user: schemas.UserRead):
-    db_decision_model = DecisionModel(
-        **decision_model.dict(), owner_id=user.id)
-    db.add(db_decision_model)
-    await db.flush()
-    await db.commit()
-    return db_decision_model
+    q = select(DecisionModel).where(
+        DecisionModel.owner_id == user.id).where(
+        DecisionModel.name == decision_model.name)
+    result = await db.execute(q)
+    result_list = result.scalars().all()
+    if not len(result_list):
+        db_decision_model = DecisionModel(
+            **decision_model.dict(), owner_id=user.id)
+        db.add(db_decision_model)
+        await db.flush()
+        await db.commit()
+        return db_decision_model
+    else:
+        result_list[0].content = decision_model.content
+        await db.flush()
+        await db.commit()
+        return result_list[0]
