73163780c2ef9287116e0c73d3ef808f9648d72e
Thomas Low <code@thomaslow.com>
2024-12-19 15:00:07 +0000

    Fix eslint issue.

--- code/frontend/src/electron/preload/index.ts
+++ code/frontend/src/electron/preload/index.ts
@@ -1,24 +1,22 @@
-import { contextBridge, ipcRenderer } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
+import { contextBridge, ipcRenderer } from "electron";
+import { electronAPI } from "@electron-toolkit/preload";
 
 // Custom APIs for renderer
 const api = {
-  getBackendBaseURL: () => ipcRenderer.invoke("getBackendBaseURL"),
-}
+  getBackendBaseURL: () => ipcRenderer.invoke("getBackendBaseURL")
+};
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
+    contextBridge.exposeInMainWorld("electron", electronAPI);
+    contextBridge.exposeInMainWorld("api", api);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 } else {
-  // @ts-ignore (define in dts)
-  window.electron = electronAPI
-  // @ts-ignore (define in dts)
-  window.api = api
+  (window as any).electron = electronAPI;
+  (window as any).api = api;
 }
