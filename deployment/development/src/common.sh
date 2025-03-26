923c4eaf465938c666310b9ca2995c59d0ba56cb
Thomas Low <code@thomaslow.com>
2025-03-26 13:47:28 +0100

    Fix deployment scripts for development environment with podman.

--- deployment/development/src/common.sh
+++ deployment/development/src/common.sh
@@ -13,4 +13,4 @@ DOCKER_BASE_CMD="podman"
 COMPOSE_BASE_CMD="podman-compose"
 
 COMPOSE_PROJECT="decision-support-ui"
-COMPOSE_CMD="${COMPOSE_BASE_CMD} -p ${COMPOSE_PROJECT} -f src/docker-compose.yml"
+COMPOSE_CMD="${COMPOSE_BASE_CMD} -p ${COMPOSE_PROJECT} -f $(pwd)/src/docker-compose.yml"
